7e89cb8f5aceebda2c2ea0ef6dbf8a85f7c67cde
MaynadieLilian <lilian.maynadierodriguez@ynov.com>
2025-03-20 22:24:43 +0100

    feat(Java): add function show to class Staff

--- src/main/java/fr/ynov/rpg/item/Staff.java
+++ src/main/java/fr/ynov/rpg/item/Staff.java
@@ -4,4 +4,8 @@ public class Staff extends HoldableItem{
     Staff(String name, int stat, int price,String rarity, int durability){
         super(name,stat,price,rarity,durability);
     }
+
+    public void show(){
+        System.out.println("Name: " + getName() +"\n Rarity: " + getRarity() + "\n Attack: " + getStat() +"\n Durability: " + getDurability());
+    }
 }
