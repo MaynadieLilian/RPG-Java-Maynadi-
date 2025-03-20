b4c7a58300e513e77cd23c6e510de940dbe4bc42
MaynadieLilian <lilian.maynadierodriguez@ynov.com>
2025-03-20 22:25:44 +0100

    feat(Java): add function show to class Sword

--- src/main/java/fr/ynov/rpg/item/Sword.java
+++ src/main/java/fr/ynov/rpg/item/Sword.java
@@ -4,4 +4,8 @@ public class Sword extends HoldableItem{
     Sword(String name, int stat, int price,String rarity, int durability){
         super(name,stat,price,rarity,durability);
     }
+
+    public void show(){
+        System.out.println("Name: " + getName() +"\n Rarity: " + getRarity() + "\n Attack: " + getStat() +"\n Durability: " + getDurability());
+    }
 }
