059884fca2986f0c600c27b00a8b497529e19f4d
MaynadieLilian <lilian.maynadierodriguez@ynov.com>
2025-03-22 04:12:58 +0100

    feat(Java): add function to class Hero

--- src/main/java/fr/ynov/rpg/character/Hero.java
+++ src/main/java/fr/ynov/rpg/character/Hero.java
@@ -1,7 +1,30 @@
 package fr.ynov.rpg.character;
+import fr.ynov.rpg.inventory.Inventory;
+import fr.ynov.rpg.item.Item;
+import fr.ynov.rpg.item.Potion;
 
 public abstract class Hero extends Character {
+    private Inventory  inventory;
+
     Hero(String name){
         super(name,100,10,50);
+        this.inventory = new Inventory();
+    }
+
+    public void pickup(Item item){
+        this.inventory.addItem(item);
+    }
+
+    public void throwAway(int index){
+        Item item = this.inventory.removeItem(index);
+        item = null;
+    }
+
+    public void drink(Potion potion){
+        potion.drink();
+    }
+
+    public Inventory getInventory(){
+        return this.inventory;
     }
 }
