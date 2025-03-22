2e118f49952de94c94522670f071d729ab13cddc
MaynadieLilian <lilian.maynadierodriguez@ynov.com>
2025-03-22 04:40:49 +0100

    feat(Java): add function to class Paladin

--- src/main/java/fr/ynov/rpg/character/Paladin.java
+++ src/main/java/fr/ynov/rpg/character/Paladin.java
@@ -1,7 +1,72 @@
 package fr.ynov.rpg.character;
 
+import fr.ynov.rpg.item.Shield;
+import fr.ynov.rpg.item.Sword;
+
 public class Paladin extends Hero {
+    private Sword sword;
+    private Shield shield;
+
     Paladin(String name){
         super(name);
+        this.sword = null;
+        this.shield = null;
+    }
+
+    public void equipSword(Sword sword){
+        this.sword = sword;
+        setAttack(this.getAttack()+sword.getStat());
+    }
+
+    public void unequipSword(Sword sword){
+        this.sword = null;
+        sword = null;
+    }
+
+    public void equipShield(Shield shield){
+        this.shield = shield;
+    }
+
+    public void unequipShield(Shield shield){
+        this.shield = null;
+        shield = null;
+    }
+
+    public void takeDamage(int damage){
+        if(this.shield != null){
+            super.takeDamage(damage-this.shield.getStat());
+            this.shield.setDurability(this.shield.getDurability()-1);
+        }else{
+            super.takeDamage(damage);
+        }
+    }
+
+    public void attack(Character character){
+        character.takeDamage(this.getAttack());
+        if(this.sword != null){
+            this.sword.setDurability(this.sword.getDurability()-1);
+        }
+        if(character.getHealth() == 0){
+            kill(character);
+        }
+    }
+
+    public void show(){
+        System.out.println("Name: "+this.getName()+"\nHealth: "+this.getHealth()+"\nAttack: "+this.getAttack()+"\nGold: "+this.getGold()+"\nSword: ");
+        if(this.sword != null){
+            System.out.println(this.sword.getName());
+        }else {
+            System.out.println("No Sword");
+        }
+        System.out.println("\nShield: ");
+        if(this.shield != null){
+            System.out.println(this.shield.getName());
+        }else{
+            System.out.println("No Shield");
+        }
+        System.out.println("\nInventory: ");
+        for(int i =0;i<this.getInventory().getItems().size();i++){
+            System.out.println(this.getInventory().getItems().get(i).getName()+" ");
+        }
     }
 }
