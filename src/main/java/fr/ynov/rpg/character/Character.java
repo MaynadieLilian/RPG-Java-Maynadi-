c024eda6815ae48cc3b7f3d502546d6ccce8050a
MaynadieLilian <lilian.maynadierodriguez@ynov.com>
2025-03-22 03:49:37 +0100

    feat(Java): add function to class Character

--- src/main/java/fr/ynov/rpg/character/Character.java
+++ src/main/java/fr/ynov/rpg/character/Character.java
@@ -12,4 +12,55 @@ public abstract class Character {
         this.attack = attack;
         this.gold = gold;
     }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getHealth(){
+        return this.health;
+    }
+
+    public void setHealth(int health){
+        this.health = health;
+    }
+
+    public int getAttack(){
+        return this.attack;
+    }
+
+    public void setAttack(int attack){
+        this.attack = attack ;
+    }
+
+    public int getGold(){
+        return this.gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public boolean isAlive(){
+        return this.health > 0;
+    }
+
+    public void takeDamage(int damage){
+        this.health -= damage;
+        if(this.health <= 0){
+            this.health = 0;
+        }
+    }
+
+    public void attack(Character target){
+        target.takeDamage(this.attack);
+    }
+
+    public void kill(Character target){
+        this.gold += target.getGold();
+    }
+
+    public void show(){
+        System.out.println("Name: "+this.name+"\nHealth: "+this.health+"\nAttack: "+this.attack);
+    }
 }
