657bf895509d31aeb55c6b6516f1d862eaa8816c
MaynadieLilian <lilian.maynadierodriguez@ynov.com>
2025-03-22 04:04:46 +0100

    feat(Java): add function to class Item

--- src/main/java/fr/ynov/rpg/inventory/Inventory.java
+++ src/main/java/fr/ynov/rpg/inventory/Inventory.java
@@ -1,16 +1,47 @@
 package fr.ynov.rpg.inventory;
 import fr.ynov.rpg.item.Item;
-import fr.ynov.rpg.item.Potion;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Inventory {
     private List<Item> items;
-    private int space;
+    private int spaces;
 
     public Inventory(){
         this.items = new ArrayList<>();
-        this.space = 5;
+        this.spaces = 5;
+    }
+
+    public List<Item> getItems(){
+        return this.items;
+    }
+
+    public Item getItem(int index){
+        if(this.items.size() > index){
+            return this.items.get(index);
+        }
+        System.out.println("this item don't exist");
+        return null;
+    }
+
+    public boolean addItem(Item item){
+        if(this.items.size() < this.spaces && item.getOwner()==null){
+            this.items.add(item);
+            return true;
+        }
+        return false;
+    }
+
+    public Item removeItem(int index){
+       Item item = this.items.get(index);
+       if(this.items.size() > 0){
+           this.items.remove(index);
+       }
+       return  item;
+    }
+
+    public void setSpaces(int space){
+        this.spaces = space;
     }
 }
